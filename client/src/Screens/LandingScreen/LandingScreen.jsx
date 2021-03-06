997e5297e1bea84d43307f8439c4276281fadad9
Gary <garygrant693@gmail.com>
2021-03-06 00:47:51 -0500

    added fadin feature

--- client/src/Screens/LandingScreen/LandingScreen.jsx
+++ client/src/Screens/LandingScreen/LandingScreen.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './LandingScreen.css'
 import Layout from '../../Components/Layout/Layout'
-
+import fadin from 'fadin'
 
 const LandingScreen = () => {
+
+    //this function is creating the delay in the photos
+    document.addEventListener('DOMContentLoaded', () => {
+        fadin('.image', { delay: 200 })
+    })
+
+    document.addEventListener('DOMContentLoaded', () => {
+        fadin('#test', { delay: 200 })
+    })
     return (
         <React.Fragment>
             <Layout>
@@ -16,18 +25,16 @@ const LandingScreen = () => {
                     <h1 className='feature-title'>Featured News</h1>
                 </div>
                 <section className='section-container'>
-                    <div id='test'></div>
-                    <div id='test'></div>
-                    <div id='test'></div>
-                    <div id='test'></div>
-
+                    <div id='test' data-delay='0.4'></div>
+                    <div id='test' data-delay='0.4'></div>
+                    <div id='test' data-delay='0.4'></div>
+                    <div id='test' data-delay='0.4'></div>
                 </section>
-                {/* <hr /> */}
                 <section className='section-container2'>
-                    <div id='test'></div>
-                    <div id='test'></div>
-                    <div id='test'></div>
-                    <div id='test'></div>
+                    <div id='test' data-delay='0.4'></div>
+                    <div id='test' data-delay='0.4'></div>
+                    <div id='test' data-delay='0.4'></div>
+                    <div id='test' data-delay='0.4'></div>
                 </section>
                 <div className='mustRead-container'>
                     <h1 className='must-read'>Must Read</h1>
