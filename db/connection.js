071805ea7fad62a0925b7b6ef68082a1522a6d8f
Gary <garygrant693@gmail.com>
2021-03-08 14:17:12 -0500

    changed mondoDB port number"

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-let MONGODB_URI = process.env.PROD_MONGODB || 'mongodb://127.0.0.1:27017/envy-us'
+let MONGODB_URI = process.env.PROD_MONGODB || 'mongodb://127.0.0.1:25283/envy-us'
 
 mongoose
     .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
