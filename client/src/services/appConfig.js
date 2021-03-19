6209e9293b32eb940cdd8de44a1ffcfb2eecbe35
Gary <garygrant693@gmail.com>
2021-03-19 00:27:00 -0400

    added correct heroku url

--- client/src/services/appConfig.js
+++ client/src/services/appConfig.js
@@ -5,10 +5,11 @@ let apiUrl
 
 const apiUrls ={
     //when the application is on surge or netlify 
-    production: 'https://git.heroku.com/envy-us-gary--app.git',
+    production: '  https://envy-us-gary--app.herokuapp.com/api',
     //the the application is on the local host
     development: 'http://localhost/3000'
 }
+//https://git.heroku.com/envy-us-gary--app.git
 
 
 if (window.location.hostname === 'localhost') {
