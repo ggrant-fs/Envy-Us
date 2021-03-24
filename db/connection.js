1281f7b0fe55dfec4d8a69076a41bf8bb1217954
Gary <garygrant693@gmail.com>
2021-03-24 02:46:02 -0400

    save

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-let MONGODB_URI = process.env.PROD_MONGODB || 'mongodb://127.0.0.1:27017/envyus'
+let MONGODB_URI = process.env.PROD_MONGODB || 'mongodb://127.0.0.1:27017/envyUsDatabase'
 
 mongoose
     .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
