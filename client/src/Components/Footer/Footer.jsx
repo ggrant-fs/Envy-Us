82398bd366c928a0e41b42d1936faf2d00937465
Gary <garygrant693@gmail.com>
2021-04-12 15:09:08 -0400

    added to footer form

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -22,9 +22,17 @@ const Footer = () => {
 
     return (
         <React.Fragment>
-            <footer onSubmit={handleSubmit}>
+            <footer>
 
-                <form>
+                <form onSubmit={handleSubmit}>
+                    <div className='form-title'>
+                        <div className='title-div'>
+                            <div>
+                                <h1 > Don't Miss a Beat</h1>
+                                {/* <p>Sign up for the latest news in fashion and culture</p> */}
+                            </div>
+                        </div>
+                    </div>
                     <lable htmlfor='email'></lable>
                     <div className='input-container'>
                         <input
@@ -35,6 +43,7 @@ const Footer = () => {
                             onChange={handleChange}
                             placeHolder='Enter Your Email Address'
                         />
+
                         <lable></lable>
                         <input
                             className='form-input'
@@ -44,11 +53,23 @@ const Footer = () => {
                             onChange={handleChange}
                             placeHolder='Enter Your Name'
                         />
+
                     </div>
                     <div className='button-container'>
                         <div>
                             <button type='submit' id='submit-button'>Sign Up</button>
                         </div>
+                    </div>
+
+                    <div className='terms-of-service'>
+                        <p id='terms'>
+                            Lorem ipsum dolor sit amet, consectetur
+                            adipiscing elit, sed do eiusmod tempor
+                            incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud
+                            exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat.
+                        </p>
                     </div>
                 </form>
             </footer>
